23779d5a22c5c76f53a1d9393df23db39d781ed3
VKamble61 <varsha.kamble61@gmail.com>
2022-09-12 12:33:01 +0100

    First Sample Testcase

---
+++ PairingExcercise/SortedStringArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairingExcercise
+{
+    public class SortedStringArray
+    {
+         
+        
+        public SortedStringArray()
+        { }
+
+        public string SortedStrings(string strArray)
+        { 
+
+            return strArray;
+        
+        }
+    }
+}
--- PairingExcercise_Test/UnitTest1.cs
+++ PairingExcercise_Test/UnitTest1.cs
@@ -1,15 +1,31 @@
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using PairingExcercise;
 namespace PairingExcercise_Test;
-
+using NUnit.Framework;
+using FluentAssertions;
 public class Tests
 {
+
+    //Given a string of words that are separated by a space, you need to return an array of the words, sorted alphabetically by the final character in each.
+    //If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+    //For example an input might be "how are you  "   ==> output are  you how 
+    // input might be "how are you same "   ==> output are same you how 
+
+    private SortedStringArray SortedStringArray;
     [SetUp]
     public void Setup()
     {
+        SortedStringArray = new SortedStringArray();
+
     }
 
     [Test]
-    public void Test1()
+    public void String_should_return_final_characher_sorted_array()
     {
-        Assert.Pass();
+        //Assert.Pass();
+        SortedStringArray.SortedStrings("how are you").Should().Be("are you how");
+
+
     }
 }
